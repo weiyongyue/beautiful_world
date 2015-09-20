34c4a291ecf41e1a15096cec4f9b9a6a38a8de2d
weiyongyue <503642650@qq.com>
2015-09-20 12:29:13 +0800

    获取 所有多媒体文件 工具

---
+++ ruby tool/find_path_filelist.rb
@@ -0,0 +1,28 @@
+
+#!/usr/bin/ruby
+require  'find'
+
+$trans
+
+def multimedea_file_filter(path)
+    if ((path =~ /.*\.rm\Z/) ||(path =~ /.*\.rmvb\Z/) \
+        ||(path =~ /.*\.mp4\Z/) \
+        ||(path =~ /.*\.avi\Z/) \
+        ||(path =~ /.*\.mpeg\Z/))
+        puts path
+        $trans.puts path
+    end
+end
+
+$trans = File.open("mv_list.txt","w")
+
+Find.find("E:\\down 文件") do |filename|
+  multimedea_file_filter(filename)
+end
+
+Find.find("E:\\迅雷下载") do |filename|
+  multimedea_file_filter(filename)
+end
+
+$trans.close
+
